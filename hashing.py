280ad04a4881ea14c302e96066ca8ee1b65d745a
lsheynova <91351017+lsheynova@users.noreply.github.com>
2021-11-20 06:46:25 +0000

    create hashing.py
    not much to it... just a temporary file to interact with different implementations of PoW and hashing at the moment.

---
+++ hashing.py
@@ -0,0 +1,24 @@
+import hashlib, random, json
+
+def randstrhash(algo):
+    h = hashlib.new(algo)
+    h.update(f"{random.getrandbits(128)}".encode('utf-8'))
+    return h.hexdigest()
+  
+def hashcontents(algo, contents):
+    h = hashlib.new(algo)
+    h.update(json.dumps(contents).encode('utf-8')))
+    return h.hexdigest()
+    
+def randstrhash(algo):
+    h = hashlib.new(algo)
+    h.update(f"{random.getrandbits(128)}".encode('utf-8'))
+    return h.hexdigest()
+
+# def proof_of_work(difficulty, contents):
+#     req_hash = hashcontents('sha256', contents)
+#     while rhash[:difficulty] != req_hash[:difficulty]:
+#         rhash = randstrhash('sha256')
+#     return True
+
+
