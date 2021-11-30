c05dd2e81a6e27abbb5c4166a4a3faa5e636f368
lsheynova <91351017+lsheynova@users.noreply.github.com>
2021-11-30 11:25:15 +0000

    lazy duplicate
    squashed

--- hashing.py
+++ hashing.py
@@ -9,12 +9,7 @@ def hashcontents(algo, contents):
     h = hashlib.new(algo)
     h.update(json.dumps(contents).encode('utf-8')))
     return h.hexdigest()
-    
-def randstrhash(algo):
-    h = hashlib.new(algo)
-    h.update(f"{random.getrandbits(128)}".encode('utf-8'))
-    return h.hexdigest()
-
+  
 # def proof_of_work(difficulty, contents):
 #     req_hash = hashcontents('sha256', contents)
 #     while rhash[:difficulty] != req_hash[:difficulty]:
